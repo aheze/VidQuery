b315f8e006d9e34fe2b4a1f25f583a20000b5ac0
Zheng <ahzzheng@gmail.com>
2021-02-26 20:45:58 -0800

    Work on grid layout

---
+++ VidQuery/Cards.swift
@@ -0,0 +1,64 @@
+//
+//  Cards.swift
+//  VidQuery
+//
+//  Created by Zheng on 2/26/21.
+//
+
+import SwiftUI
+
+struct FeatureCard: View {
+    var image: Image = Image("sample")
+    var title: String = "Title"
+    
+    var body: some View {
+        VStack {
+            image
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+            
+            
+            Text(title)
+                .font(.system(size: 19, weight: .medium))
+                .padding(EdgeInsets(top: 6, leading: 6, bottom: 12, trailing: 6))
+            
+        }
+        .frame(maxWidth: .infinity)
+        .background(
+            Color.white
+        )
+        .cornerRadius(8) .shadow(color: Color(#colorLiteral(red: 0.5723067522, green: 0.5723067522, blue: 0.5723067522, alpha: 0.5)), radius: 5, x: 0.0, y: 2)
+    }
+}
+
+struct CategoryCard: View {
+    var name: String = "Title"
+    
+    var body: some View {
+        VStack {
+            
+            Text(name)
+                .font(.system(size: 19, weight: .medium))
+                .padding(12)
+            
+        }
+        .frame(maxWidth: .infinity)
+        .background(
+            Color.white
+        )
+        .cornerRadius(8) .shadow(color: Color(#colorLiteral(red: 0.5723067522, green: 0.5723067522, blue: 0.5723067522, alpha: 0.5)), radius: 5, x: 0.0, y: 2)
+    }
+}
+
+struct FeatureCard_Previews: PreviewProvider {
+    static var previews: some View {
+        FeatureCard()
+    }
+}
+
+
+struct CategoryCard_Previews: PreviewProvider {
+    static var previews: some View {
+        CategoryCard()
+    }
+}
--- VidQuery/ContentView.swift
+++ VidQuery/ContentView.swift
@@ -13,10 +13,20 @@ struct Media: Identifiable {
     let title: String
 }
 
+struct Category: Identifiable {
+    let id = UUID()
+    
+    let name: String
+}
+
 struct ContentView: View {
     
     let columns = [
-        GridItem(.adaptive(minimum: 150))
+        GridItem(.adaptive(minimum: 150), spacing: 24)
+    ]
+    
+    let categoryColumns = [
+        GridItem(.adaptive(minimum: 150), spacing: 24)
     ]
     
     var media = [
@@ -29,26 +39,58 @@ struct ContentView: View {
         Media(title: "sdfsdfsdfsdf")
     ]
     
+    var categories = [
+        Category(name: "Gaming"),
+        Category(name: "Tech"),
+        Category(name: "Cartoons"),
+        Category(name: "Comedy"),
+        Category(name: "Slice of life"),
+        Category(name: "Action"),
+        Category(name: "Gaming"),
+        Category(name: "Tech"),
+        Category(name: "Cartoons"),
+        Category(name: "Comedy"),
+        Category(name: "Slice of life"),
+        Category(name: "Action"),
+        Category(name: "Horror")
+    ]
+    
+    
     var body: some View {
-        NavigationView {
-            
-            VStack {
-                Text("Featured")
-                    .foregroundColor(Color(#colorLiteral(red: 0.6621153355, green: 0.6622314453, blue: 0.6621080041, alpha: 1)))
-                    .font(.system(size: 24, weight: .medium))
+        
+        ScrollView {
+            VStack(alignment: .leading) {
+                HStack {
+                    Text("Featured")
+                        .foregroundColor(Color(#colorLiteral(red: 0.6621153355, green: 0.6622314453, blue: 0.6621080041, alpha: 1)))
+                        .font(.system(size: 24, weight: .medium))
+                        .padding(EdgeInsets(top: 16, leading: 0, bottom: 6, trailing: 0))
+                }
                 
-                LazyVGrid(columns: columns, alignment: .leading, spacing: 20) {
-                    
+                LazyVGrid(columns: columns, alignment: .leading, spacing: 24) {
                     ForEach(media) { media in
-                        FeatureCard(image: Image(systemName: "folder"), title: media.title)
+                        FeatureCard(title: media.title)
                     }
                 }
                 
+                HStack {
+                    Text("Categories")
+                        .foregroundColor(Color(#colorLiteral(red: 0.6621153355, green: 0.6622314453, blue: 0.6621080041, alpha: 1)))
+                        .font(.system(size: 24, weight: .medium))
+                        .padding(EdgeInsets(top: 16, leading: 0, bottom: 6, trailing: 0))
+                }
+                
+                LazyVGrid(columns: categoryColumns, alignment: .leading, spacing: 24) {
+                    ForEach(categories) { category in
+                        CategoryCard(name: category.name)
+                    }
+                }
+                
+                
+                
             }
-            
-            
+            .padding(24)
         }
-        .navigationTitle("VidQuery")
         .frame(minWidth: 350, maxWidth: .infinity, minHeight: 300, maxHeight: .infinity)
     }
 }
@@ -56,5 +98,7 @@ struct ContentView: View {
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+            .previewLayout(.fixed(width: 1000, height: 600))
+            
     }
 }
